5e72962c27532add095b017d25f113fc6e5949b1
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-06-02 00:49:10 +0330

    displaying uploaded songs
    fetching uploaded songs by the user and displaying it

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 export default async function Home() {
   const songs = await getSongs();
   return (
-    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <div className="bg-neutral-900 md:rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import MediaItem from "./MediaItem";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const { user } = useUser();
   const uploadModal = useUploadModal();
@@ -30,7 +36,11 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
