e3591c7ee48c88d0689ed0766fd3a90c9a3ec6fb
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-06-02 15:13:15 +0330

    added search functionality
    search functionality using query string added

---
+++ components/SearchContent.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+
+interface SearchContentProps {
+  songs: Song[];
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No songs found.
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gapx-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem data={song} onClick={() => {}} />
+          </div>
+          {/* TODO:add like button here */}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default SearchContent;
